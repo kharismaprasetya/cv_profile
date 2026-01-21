cdee4c924229fbcca6ac78d41cedf64646ad35aa
Kharisma Prasetya <kharisma.prasetya@gmail.com>
2026-01-21 17:25:44 +0700

    DONE finished handle responsive in introduction

--- components/introduction/Introduction.tsx
+++ components/introduction/Introduction.tsx
@@ -4,16 +4,17 @@ import { FaDownload } from "react-icons/fa6";
 export default function Introduction() {
   return (
     <section className="introduction h-dvh">
-      <div className="bg-grid grid grid-cols-2 gap-4 py-[15vh] px-[60px] h-dvh">
+      <div className="bg-grid md:grid md:grid-cols-2 md:gap-4 py-4 md:py-[10vh] px-8 md:px-[60px] h-dvh">
         <div className="relative">
-          <div className="introduction__subtitle font-medium mb-[1rem] text-indigo-400 text-lg">
+          <div className="introduction__subtitle font-medium mb-[1rem] text-indigo-400 text-base md:text-lg">
             <p>Hey Hi! 👋</p>
           </div>
-          <h1 className="text-white text-5xl font-bold leading-[1.2]">
+          <h1 className="text-white text-xl lg:text-4xl xl:text-5xl font-bold md:leading-[1.2]">
             I'm Kharisma Prasetya
             <br />
             <span className="flex items-center gap-4">
               <Image
+                className="w-[44px] md:w-[56px] lg:w-[64px]"
                 width={64}
                 height={64}
                 src="/icons/diamond-star.svg"
@@ -22,7 +23,7 @@ export default function Introduction() {
               <span className="text-[#C4EF17]">Software Engineer</span>
             </span>
           </h1>
-          <p className="mt-2">
+          <p className="mt-2 text-xs md:text-sm lg:text-base md:leading-[1.7] lg:leading-[1.625]">
             I am a Fullstack Software Engineer with over 7 years of experience
             in building web and mobile applications. I specialize in frontend
             development using Vue.js, Nuxt.js, React.js, Next.js, and React
@@ -34,15 +35,38 @@ export default function Introduction() {
             creating clean, maintainable code and delivering meaningful products
             that provide real value.
           </p>
-          <a
-            href="/pdf/cv_kharisma_prasetya.pdf"
-            download="cv_kharisma_prasetya.pdf"
-            className="btn btn-primary mt-4 inline-flex items-center gap-2"
-          >
-            <span>Download CV</span>
-            <FaDownload />
-          </a>
-          <div className="absolute top-[-10%] right-[4%]">
+          <div className="block md:hidden relative flex justify-center items-center mt-4">
+            <div className="absolute top-0 left-0 w-full h-full rounded-[50%] bg-[rgba(196,239,23,.2)] blur-[128px] -z-1"></div>
+            <Image
+              className="h-[300px] w-auto"
+              width={200}
+              height={300}
+              sizes="100vw"
+              src="/images/profile.png"
+              alt="Kharisma Prasetya"
+              preload
+            />
+            <div className="absolute top-0 left-[10%]">
+              <Image
+                className="w-[70px] animate-spin [animation-duration:6s] opacity-50"
+                width={70}
+                height={70}
+                src="/icons/diamond-star2.svg"
+                alt="Diamond Star"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center md:justify-start">
+            <a
+              href="/pdf/cv_kharisma_prasetya.pdf"
+              download="cv_kharisma_prasetya.pdf"
+              className="btn btn-primary mt-4 inline-flex items-center gap-2"
+            >
+              <span>Download CV</span>
+              <FaDownload />
+            </a>
+          </div>
+          <div className="absolute top-0 right-0 md:top-[-10%] md:right-[4%]">
             <Image
               className="w-[90px] animate-left-right"
               width={90}
@@ -51,7 +75,7 @@ export default function Introduction() {
               alt="Loop Arrow"
             />
           </div>
-          <div className="absolute bottom-[-15%] right-[10%]">
+          <div className="absolute bottom-[-10%] left-0 md:bottom-[-15%] md:right-[10%]">
             <Image
               className="w-[70px] animate-rotate opacity-50"
               width={70}
@@ -63,7 +87,7 @@ export default function Introduction() {
           <div className="absolute bottom-[-50%] left-[-25%] w-[300px] h-[300px] rounded-[50%] bg-[rgba(196,239,23,.2)] blur-3xl -z-1"></div>
         </div>
         <div className="relative">
-          <div className="absolute top-[-10%] right-[4%]">
+          <div className="hidden md:block absolute top-[-10%] right-[4%]">
             <Image
               className="w-[70px] animate-spin [animation-duration:6s] opacity-50"
               width={70}
@@ -72,8 +96,8 @@ export default function Introduction() {
               alt="Diamond Star"
             />
           </div>
-          <div className="absolute top-0 left-0 w-full h-full rounded-[50%] bg-[rgba(196,239,23,.2)] blur-[128px] -z-1"></div>
-          <div className="flex justify-center items-center h-full">
+          <div className="hidden md:block absolute top-0 left-0 w-full h-full rounded-[50%] bg-[rgba(196,239,23,.2)] blur-[128px] -z-1"></div>
+          <div className="hidden md:block flex justify-center items-center h-full">
             <Image
               className="h-[450px] w-auto"
               width={246}
@@ -87,7 +111,7 @@ export default function Introduction() {
         </div>
         <div className="absolute bottom-0 right-0">
           <Image
-            className="w-[250px] animate-spin [animation-duration:8s]"
+            className="w-[150px] md:w-[250px] animate-spin [animation-duration:8s]"
             width={250}
             height={250}
             src="/icons/sixline-asterisk.svg"
