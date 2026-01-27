2cbcb878138f1b39c3d126d3e3f2d7feb0e275f5
Kharisma Prasetya <kharisma.prasetya@gmail.com>
2026-01-27 09:34:15 +0700

    DONE finished handle metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-// import Image from "next/image";
+import type { Metadata } from 'next'
 import Experience from "@/components/experience/Experience";
 import Footer from "@/components/footer/Footer";
 import Introduction from "@/components/introduction/Introduction";
+
+export const metadata: Metadata = {
+  title: "Kharisma Prasetya - CV Profile",
+  description: "Kharisma Prasetya - CV Profile",
+  openGraph: {
+    title: "Kharisma Prasetya - CV Profile",
+    description: "Kharisma Prasetya - CV Profile",
+    url: "https://kharismaprasetya.vercel.app/",
+    siteName: "Kharisma Prasetya",
+    images: [
+      {
+        url: "https://kharismaprasetya.vercel.app/images/profile.png",
+        width: 1200,
+        height: 630,
+        alt: "Kharisma Prasetya - CV Profile",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+}
 
 export default function Home() {
   return (
